ace5e0b79f5f349e6100b596c97333fe74d01cfd
Anna <new@new.ru>
2021-08-20 23:30:21 +0700

    небольшая доработка теста

--- siberianHealthTests/src/test/java/com/siberianhealth/ru/appmanager/HelperMethods.java
+++ siberianHealthTests/src/test/java/com/siberianhealth/ru/appmanager/HelperMethods.java
@@ -31,10 +31,8 @@ public class HelperMethods {
       }
    }
 
-   public String selectAnyItem(int num) {
+   public String getItemName(int num) {
       return getText(By.xpath("//div[@ec-product=\"product\"][" + num + "]//a[@itemprop=\"name\"]"));
-
-
    }
 
    public void putInCart(String itemName) {
@@ -44,22 +42,12 @@ public class HelperMethods {
       Actions actions = new Actions(driver);
       WebElement target = driver.findElement(By.xpath(xpathButtonPutInCart));
       actions.moveToElement(target).perform();
-
       click(By.xpath(xpathButtonPutInCart));
-
    }
 
    public void goToCart() {
       String xpathGoToCart = "//a[@href=\"/ru/shop/cart/\"]";
       click(By.xpath(xpathGoToCart));
-   }
-
-   public void checkBasketNotEmpty(String itemName, int i) {
-      String xpath = "//div[contains(@class, 'cart-package ng-scope')]" +
-              "//a[contains(@class, 'os-product-card__title') and contains(text(), \"" + itemName + "\")]";
-      Assert.assertTrue(isElementPresent(By.xpath(xpath)));  //проверка по названию
-      String xpath2 = "//div[@ng-bind-html=\"userService.cart.amount\"]";
-      Assert.assertEquals(Integer.parseInt(driver.findElement(By.xpath(xpath2)).getText()), i);  //проверка по количеству
    }
 
    public void click(By locator) {
@@ -84,4 +72,43 @@ public class HelperMethods {
       }
    }
 
+
+
+   public int getItemPrice(int num) {
+      String xpathItemPrice = "//div[@ec-product='product']["+num+"]//span[contains(@ng-bind-html, \"product.price.current\")]/span[1]";
+      System.out.println(xpathItemPrice);
+      return Integer.parseInt(getText(By.xpath(xpathItemPrice)));
+   }
+
+   public void checkNameItemInCart(String itemName) {
+      String xpathName = "(//div[@class=\"cart-package__content\"]//div[contains(@class, \"column _name\")])[1]" +
+              "//a[contains(@class, 'os-product-card__title') and contains(text(), \"" + itemName + "\")]";
+      Assert.assertTrue(isElementPresent(By.xpath(xpathName)));  //проверка по названию
+   }
+
+   public void checkPriceInCart(int price) {
+      String xpathItemPriceInCart = "(//div[@class=\"cart-package__content\"]//div[contains(@class, \"cart-package__item__price\")])[1]";
+      int priceInCart = Integer.parseInt(getText(By.xpath(xpathItemPriceInCart)));
+      Assert.assertEquals(priceInCart, price);
+   }
+
+   public void checkCountInCart(int count) {
+      String xpathCount = "(//div[@class=\"cart-package__content\"]//div[contains(@class, \"column _count\")])[1]//input";
+      String tmp = driver.findElement(By.xpath(xpathCount)).getAttribute("value");
+      int countInCart = Integer.parseInt((tmp).replaceAll("[^0-9]", ""));
+      Assert.assertEquals(countInCart, count);  //проверка по количеству
+   }
+
+   public void checkTotalCountInCart(int count) {
+      String xpathTotalCountInCart ="//div[@ng-bind-html=\"userService.cart.amount\"]";
+      int totalCountInCart = Integer.parseInt(getText(By.xpath(xpathTotalCountInCart)));
+      Assert.assertEquals(totalCountInCart, count);
+
+   }
+
+   public void checkTotalPriceInCart(int price) {
+      String xpathTotalPriceInCart ="//div[contains(@class, \"total-cart-block__price\")]";
+      int totalPriceInCart = Integer.parseInt(getText(By.xpath(xpathTotalPriceInCart)));
+      Assert.assertEquals(totalPriceInCart, price);
+   }
 }
--- siberianHealthTests/src/test/java/com/siberianhealth/ru/tests/Test01CheckingItemInCart.java
+++ siberianHealthTests/src/test/java/com/siberianhealth/ru/tests/Test01CheckingItemInCart.java
@@ -7,21 +7,31 @@ import org.testng.annotations.Test;
 public class Test01CheckingItemInCart extends TestsBase {
 
 
-   String itemName;   //что буду искать и добавлять в корзину
+   String itemName;     //здесь будем хранить название товара,
+                        // извлекается по порядковому номеру num на странице Красота
+   int num = 3;         //добавляем например 3й товар товар
+   int count = 1;       //количество товара
+   int price;           //здесь будем хранить цену товара
+
 
    @Test
    public void test01CheckingItemInCart() {
       app.getURLWebsite();                                    // зайти на сайт https://ru.siberianhealth.com/ru/
       app.getHelper().closePopUpWindow();                      // закрыть всплывающее информационное окошко, если выскакивает
       app.getHelper().goToBeautyCategory();                   // Зайти в категорию Красота
-      itemName = app.getHelper().selectAnyItem(3);       // Выбрать некий товар  (3й по списку в разделе красота, вернуть название)
+      itemName = app.getHelper().getItemName(num);           // Выбрать некий товар  (3й по списку в разделе красота, вернуть название)
+      price = app.getHelper().getItemPrice(num);
       app.getHelper().putInCart(itemName);                    // Добавить выбранный товар в корзину
       app.getHelper().goToCart();                             // Перейти в корзину
-      app.getHelper().checkBasketNotEmpty(itemName, 1);     // Проверить что корзина не пустая (/присутствует выбранный товар в количестве 1 единица)
+      app.getHelper().checkNameItemInCart(itemName);        // Проверить что корзина не пустая (/присутствует выбранный товар по имени)
+      app.getHelper().checkCountInCart(count);              //проверка что совпадает количество
+      app.getHelper().checkPriceInCart(price);              // проверка что совпадает цена
 
+      ////////////////  Итого в корзине
+      app.getHelper().checkTotalCountInCart(count);           //проверка общего количества товаров
+      app.getHelper().checkTotalPriceInCart(price);           // проверка поля "Итого к оплате"
 
    }
-
 
 
 
